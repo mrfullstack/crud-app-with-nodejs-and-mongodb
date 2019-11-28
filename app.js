0dc1e161c9d731097331034ee8215dac5a0dadaf
Guru <djguruwap@gmail.com>
2019-11-28 15:08:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+//import DB and routes
+const db = require('./config/DB').MONGO_URI
+const userRoutes = require('./routes/userRoutes')
+
+//port
+const port = 4000 || process.env.PORT
+
+//middleweres
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(helmet())
+
+
+//use routes
+app.use(userRoutes)
+
+
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('DB CONNECTED')
+        app.listen(port, () => {
+            console.log(`Server listening on ${port}`)
+        })
+    })
+    .catch(err => console.log(err))
+
---
+++ config/DB.js
@@ -0,0 +1,7 @@
+//Import your mongo db uri
+//i am using 
+const uri = "mongodb+srv://<username>:<password>@cluster0-vmyny.mongodb.net/test?retryWrites=true&w=majority"
+
+module.exports = {
+    MONGO_URI: uri
+}
